c9c59a6ad4661c1df979cd20f3917744849cbcb3
Andrew Timoshenko <andrewtimoshenko@klika-tech.com>
2019-04-22 23:01:52 +0300

    test 1 is passed

--- src/cpp.cpp
+++ src/cpp.cpp
@@ -20,7 +20,9 @@ int main() {
 
 	map<string, vector<string>> bus_stop_m;
 //	map<string, vector<string>> stop_bus_m;
-	vector <string> vec = {"first","second"};
+
+	vector <string> v = {"first", "second"};
+
 	cin >> Q;
 
 	for (int q = 0; q < Q; q++) {
@@ -36,40 +38,69 @@ int main() {
 				bus_stop_m[bus].push_back(str);
 			}
 
-
 		} else if (operation == "BUSES_FOR_STOP") {
-
+			uint8_t stop_exist = 0;
+			cin >> stop;
+			for (auto b: bus_stop_m){
+				for (auto s: b.second){
+					if (s == stop){
+						cout << b.first << " ";
+						stop_exist = 1;
+					}
+				}
+			}
+			if (stop_exist == 0){
+				cout << "No stop";
+			}
+			cout << endl;
 
 		} else if (operation == "STOPS_FOR_BUS") {
+			uint8_t interchange = 0;
+			uint8_t bus_exist  = 0;
+
 			cin >> bus;
-			cout << "BUS: " << bus << endl;
-			for (auto stop_name: bus_stop_m[bus]){
-				cout << stop_name << " ";
+//			auto it = bus_stop_m.begin(); // TODO: отказаться от обращения [], сделать поиск через find
+			// проверить на простых примерах
+			// NEW_BUS 1 5 q w e r t
+			// STOPS_FOR_BUS 2
+			// ALL_BUSES
+
+			for (auto s: bus_stop_m[bus]){
+
+				bus_exist = 1;
+				interchange = 0;
+				cout << "Stop " << s << ": ";
+				for (auto b: bus_stop_m){
+					for (auto st: b.second){
+						if (st == s && b.first != bus){
+							cout << b.first << " ";
+							interchange = 1;
+						}
+					}
+				}
+
+			if (interchange == 0){
+				cout << "no interchange";
+			}
+				cout << endl;
+			}
+			if (bus_exist == 0){
+				cout << "No bus" << endl;
 			}
 
 		} else if (operation == "ALL_BUSES") {
-
-
+			if (bus_stop_m.size() == 0){
+				cout << "No buses" << endl;
+			}
+			for (auto b: bus_stop_m){
+				cout << "Bus " << b.first << ": ";
+				for (auto s: b.second){
+					cout << s << " ";
+				}
+				cout << endl;
+			}
 		}
 }
 
-return 0;
+	return 0;
 }
-
-
-
-void print_map(const map<string, string>& m) {
-	cout << "Size = " << m.size() << endl;
-	for (const auto& item : m) {
-		cout << item.first << ": " << item.second << endl;
-	}
-}
-
-// Буква уже в словаре? Нет - добавляем, Да - инкремент частоты в словаре
-void char_counters(string word, map<char, int>& m) {
-	// Each letter
-	for (auto ch : word) {
-		++m[ch];
-	}
-}
-
