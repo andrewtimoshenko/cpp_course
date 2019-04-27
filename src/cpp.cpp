3840195d3d7c69b94a99010b165bb613644c15ff
timandr <andrewtimoshenko@klika-tech.com>
2019-04-27 17:41:51 +0300

    [WHITE, week 2]. Stops busses - 1.

--- src/cpp.cpp
+++ src/cpp.cpp
@@ -1,134 +1,97 @@
-#include <string>
 #include <iostream>
+#include <string>
 #include <vector>
 #include <map>
-#include <bits/stl_pair.h>
+#include <algorithm>
 
 using namespace std;
 
-void print_map(const map<string, string>& m);
-void char_counters(string word, map<char, int>& m);
 
 int main() {
+    int q;
+    cin >> q;
+    map<string, vector<string>> buses;
+    map<string, vector<string>> stops;
 
-	int Q = 0;
+    for (int i = 0; i < q; ++i) {
+        string operation_code;
+        cin >> operation_code;
+        if (operation_code == "NEW_BUS") {
+            string bus;
+            int stop_count;
+            cin >> bus;
+            cin >> stop_count;
+            vector<string> v(stop_count);
+            for (string& s : v) {
+                cin >> s;
+                vector<string> v2;
+                v2.push_back(bus);
+                if (stops.count(s)) {
+                    stops[s].insert(end(stops[s]), begin(v2), end(v2));
+                } else {
+                    stops[s] = v2;
+                }
+            }
+            buses[bus] = v;
+        } else if (operation_code == "BUSES_FOR_STOP") {
+            string stop;
+            cin >> stop;
+            if (stops[stop].size() == 0) {
+                cout << "No stop" << endl;
+                stops.erase(stop);
+            } else {
+                for (const auto& item : stops) {
+                    if (item.first == stop) {
+                        for (string w : item.second) {
+                            cout << w << " ";
+                        }
+                        cout << endl;
+                    }
+                }
+            }
+        } else if (operation_code == "STOPS_FOR_BUS") {
+            string bus;
+            cin >> bus;
+            if (buses.count(bus) > 0) {
+                for (const auto& item : buses) {
+                    if (item.first == bus) {
+                        for (string w : item.second) {
+                            cout << "Stop " << w << ": ";
+                            for (const auto& item2 : stops) {
+                                if (item2.first == w) {
+                                    if (item2.second.size() == 1) {
+                                        cout << "no interchange" << endl;
+                                    } else {
+                                        for (string w2 : item2.second) {
+                                            if (w2 != bus) {
+                                                cout << w2 << " ";
+                                            }
+                                        }
+                                        cout << endl;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                cout << "No bus" << endl;
+            }
+        } else if (operation_code == "ALL_BUSES") {
+            if (buses.size() == 0) {
+                cout << "No buses" << endl;
+            } else {
+                for (const auto& item : buses) {
+                    cout << "Bus " << item.first << ": ";
+                    for (string w : item.second) {
+                        cout << w << " ";
+                    }
+                    cout << endl;
+                }
+                cout << endl;
+            }
+        }
+    }
 
-	string operation;
-	string bus;
-	string stop;
-	int stop_count = 0;
-
-	map <string, vector<string>> bus_stop_m =
-	{
-	   {"b2", {"s2", "s1", "s3"}},
-	   {"b3", {"s1", "s2", "s3"}},
-	   {"b1", {"s1", "s2", "s3"}}
-	};
-
-	for (auto key: bus_stop_m){
-		cout << key.first << ": ";
-		for (auto value: key.second){
-			cout << value << " ";
-		}
-		cout << endl;
-	}
-
-	cout << endl;
-	auto it = bus_stop_m.end();
-	it--;
-	for (it; it != bus_stop_m.begin(); ){
-		cout << it->first << ": ";
-		for (auto value: it->second){
-			cout << value << " ";
-		}
-		cout << endl;
-		it--;
-	}
-
-	cin >> Q;
-
-	for (int q = 0; q < Q; q++) {
-
-		cin >> operation;
-
-		// NEW_BUS bus stop_count stop1 stop2 ...
-		if (operation == "NEW_BUS") {
-			cin >> bus >> stop_count;
-			string str;
-			for (int i = 1; i <= stop_count; i++){
-				cin >> str;
-				bus_stop_m[bus].push_back(str);
-			}
-
-
-		} else if (operation == "BUSES_FOR_STOP") {
-			uint8_t stop_exist = 0;
-			cin >> stop;
-			for (auto b: bus_stop_m){
-				for (auto s: b.second){
-					if (s == stop){
-						cout << b.first << " ";
-						stop_exist = 1;
-					}
-				}
-			}
-			if (stop_exist == 0){
-				cout << "No stop";
-			}
-			cout << endl;
-
-		} else if (operation == "STOPS_FOR_BUS") {
-			uint8_t interchange = 0;
-
-			cin >> bus;
-/*
-4
-NEW_BUS b2 3 s2 s1 s3
-NEW_BUS b3 3 s1 s2 s3
-NEW_BUS b1 3 s1 s2 s3
-STOPS_FOR_BUS b2
-
-Stop s2: b3 b1
-Stop s1: b3 b1
-Stop s3: b3 b1
- */
-			auto it = bus_stop_m.find(bus);
-			// Если автобус существует
-			if (it != bus_stop_m.cend()) {
-				for(auto s: it->second){
-					cout << "Stop " << s << ": ";
-					for ( auto b: bus_stop_m ){ // TODO: сделать обратный перебор автобусов
-						for ( auto st: b.second ){
-							if (st == s && b.first != bus ){
-								cout << b.first << " ";
-								interchange = 1;
-							}
-						}
-					}
-					if (interchange == 0){
-						cout << "no interchange";
-					} else{
-						interchange = 0;
-					}
-					cout << endl;
-				}
-			} else {
-				cout << "No bus" << endl;
-			}
-
-		} else if (operation == "ALL_BUSES") {
-			if (bus_stop_m.size() == 0){
-				cout << "No buses" << endl;
-			}
-			for (auto b: bus_stop_m){
-				cout << "Bus " << b.first << ": ";
-				for (auto s: b.second){
-					cout << s << " ";
-				}
-				cout << endl;
-			}
-		}
-}
-
-	return 0;
+    return 0;
 }
