5ec84c810bfaefb6b1c5042fe7bf3c24910898df
Andrew Timoshenko <andrewtimoshenko@klika-tech.com>
2019-05-01 19:53:38 +0300

    [WHITE, week 2]. Quantity of strings

--- src/cpp.cpp
+++ src/cpp.cpp
@@ -2,37 +2,33 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <algorithm>
+#include <set>
 
 using namespace std;
 
 
 int main() {
     int q;
-    uint32_t routes_counter = 0;
     cin >> q;
-    map<vector<string>, uint32_t> stops_m;
+
+    set <string> str_s;
 
     for (int i = 0; i < q; ++i) {
-        uint32_t stops_num = 0;
-        cin >> stops_num;
 
-        vector<string> v(stops_num);
+    	string str;
+    	cin >> str;
 
-        for (uint32_t i = 0; i < stops_num; i++){
-        	string stop_name;
-        	cin >> stop_name;
-        	v.push_back(stop_name);
-        }
-        auto it = stops_m.find(v);
-        if (it != stops_m.cend()){
-        	cout << "Already exists for " << it->second << endl;
-        } else {
-        	++routes_counter;
-        	cout << "New bus " << routes_counter << endl;
-        	stops_m[v] = routes_counter;
-        }
+    	str_s.insert(str);
     }
+
+    int i = 0;
+    for (auto item: str_s) {
+    	if (str_s.count(item) == 1){
+    		++i;
+    	}
+    }
+
+    cout << i;
 
     return 0;
 }
