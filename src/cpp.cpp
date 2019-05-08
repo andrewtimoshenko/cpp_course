9433e60311a2c72882f99f8ffb07dcbc3ca9e2ef
Andrew Timoshenko <andrewtimoshenko@klika-tech.com>
2019-05-08 19:31:53 +0300

    [WHITE, week 2]. test 5 is wrong

--- src/cpp.cpp
+++ src/cpp.cpp
@@ -6,29 +6,57 @@
 
 using namespace std;
 
-set<string> BuildMapValuesSet(const map<int, string>& m) {
-
-	set<string> s;
-
-	for (auto& item: m){
-		s.insert(item.second);
-	}
-
-	return s;
-}
-
 int main() {
 
-	set<string> values = BuildMapValuesSet({
-	    {1, "odd"},
-	    {2, "even"},
-	    {3, "odd"},
-	    {4, "even"},
-	    {5, "odd"}
-	});
+	uint32_t q = 0;
+	cin >> q;
 
-	for (const string& value : values) {
-	  cout << value << endl;
+	map <string, set<string>> synonyms;
+
+	for (uint32_t i = 0; i < q; i++){
+		string operation;
+		cin >> operation;
+
+		if (operation == "ADD"){ // ADD word1 word2
+			string word1, word2;
+			cin >> word1 >> word2;
+
+			if (synonyms.find(word1) != synonyms.end()){
+				synonyms[word1].insert(word2);
+			} else {
+				set<string> s;
+				s.insert(word2);
+				synonyms[word1] = s;
+
+				if (synonyms.find(word2) != synonyms.end()){
+					synonyms[word2].insert(word1);
+				} else {
+					s.clear();
+					s.insert(word1);
+					synonyms[word2] = s;
+				}
+			}
+
+		} else if (operation == "COUNT"){ // COUNT word
+			string word;
+			cin >> word;
+			if (synonyms.find(word) != synonyms.end()){
+				cout << synonyms[word].size() << endl;
+			} else {
+				cout << 0 << endl;
+			}
+
+		} else if (operation == "CHECK"){ //CHECK word1 word2
+			string word1, word2;
+			cin >> word1 >> word2;
+
+			if (((synonyms.find(word1) != synonyms.end()) && (synonyms[word1].count(word2) > 0)) ||
+				((synonyms.find(word2) != synonyms.end()) && (synonyms[word2].count(word1) > 0))){
+				cout << "YES" << endl;
+			} else {
+				cout << "NO" << endl;
+			}
+		}
 	}
 
     return 0;
