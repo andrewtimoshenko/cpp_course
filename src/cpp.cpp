005326f968c4e036a772cb5060fdd7b5f4cf82f0
Andrew Timoshenko <andrewtimoshenko@klika-tech.com>
2019-05-09 20:27:19 +0300

    [WHITE, week 2]. Bus stops - 3

--- src/cpp.cpp
+++ src/cpp.cpp
@@ -8,50 +8,31 @@ using namespace std;
 int main() {
   int q;
   cin >> q;
-
+  int route_cntr = 1;
   /*
    * Не нужно ничего усложнять!
    * Просто нужно использовать те вещи, которые дали в уроках
    * и не придумывать лишнего!
    */
 
-
-  map<string, set<string>> synonyms;
+  map <set<string>, int> routes_m;
 
   for (int i = 0; i < q; ++i) {
-    string operation_code;
-    cin >> operation_code;
+    int stops_number = 0;
+    cin >> stops_number;
 
-    if (operation_code == "ADD") {
-
-      string first_word, second_word;
-      cin >> first_word >> second_word;
-
-      // второе слово добавляем в список синонимов первого...
-      synonyms[first_word].insert(second_word);
-      // и наоборот
-      synonyms[second_word].insert(first_word);
-      // Не нужно искать есть ли уже слово в словаре или во множестве
-      // дубли не будут добавляться и контейнеры уже отсортированы!
-    } else if (operation_code == "COUNT") {
-
-      string word;
-      cin >> word;
-      cout << synonyms[word].size() << endl;
-      // для вывода кол-ва синонимов просто выводим размер множества соответствующего слова!
-    } else if (operation_code == "CHECK") {
-
-      string first_word, second_word;
-      cin >> first_word >> second_word;
-
-      // ищём второе слово во множестве синонимов первого
-      // (можно было сделать и наоборот)
-      if (synonyms[first_word].count(second_word) == 1) {
-        cout << "YES" << endl;
-      } else {
-        cout << "NO" << endl;
-      }
-      // просто по соответствующему слову ищем в его множестве синоним
+    set <string> names;
+    for (int n = 0; n < stops_number; n++){
+    	string stop_name;
+    	cin >> stop_name;
+    	names.insert(stop_name);
+    }
+    if (routes_m.find(names) != routes_m.end()){
+    	cout << "Already exists for " << routes_m[names] << endl;
+    } else {
+        routes_m[names] = route_cntr;
+        cout << "New bus " << routes_m[names] << endl;
+        route_cntr++;
     }
   }
 
--- src/readme.cpp
+++ src/readme.cpp
@@ -3,19 +3,33 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <map>
 
 using namespace std;
 
 int main() {
-  int n;
-  cin >> n;
-  set<string> strings;
-  for (int i = 0; i < n; ++i) {
-    string s;
-    cin >> s;
-    strings.insert(s);
+  int q;
+  cin >> q;
+
+  map<set<string>, int> buses;
+
+  for (int i = 0; i < q; ++i) {
+    int n;
+    cin >> n;
+    set<string> stops;
+    for (int j = 0; j < n; ++j) {
+      string stop;
+      cin >> stop;
+      stops.insert(stop);
+    }
+    if (buses.count(stops) == 0) {
+      const int new_number = buses.size() + 1;
+      buses[stops] = new_number;
+      cout << "New bus " << new_number << endl;
+    } else {
+      cout << "Already exists for " << buses[stops] << endl;
+    }
   }
-  cout << strings.size() << endl;
 
   return 0;
 }
