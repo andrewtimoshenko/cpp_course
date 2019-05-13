74f32eff021a60118088734aae8b09bcf6bfddd7
Andrew Timoshenko <andrewtimoshenko@klika-tech.com>
2019-05-13 21:24:14 +0300

    [WHITE, week 3]. Sorting non case sensitive

--- src/cpp.cpp
+++ src/cpp.cpp
@@ -15,31 +15,35 @@ int main() {
    * Просто нужно использовать те вещи, которые дали в уроках
    * и не придумывать лишнего!
    */
-  vector <int> array;
+  vector <string> strings;
   if (n >= 0 && n <= 1000){
 	  for (int i = 0; i < n; ++i) {
-		  int x;
-		  cin >> x;
-		  array.push_back(x);
-		  if (x >= -1000000 && x <= 1000000){
-		  } else {
-			  return 1;
+		  string str;
+		  cin >> str;
+		  if (str.size() >= 1 && str.size() <= 15){
+			  strings.push_back(str);
 		  }
+	  }
+
+	  sort(strings.begin(), strings.end(), [](string a, string b){ // a.size() > b.size() &&
+		  for (auto &ch: a){
+			  ch = tolower(ch);
+		  }
+		  for (auto &ch: b){
+			  ch = tolower(ch);
+		  }
+		  if(a < b) {
+			  return true;
+		  } else {
+			  return false;
+		  }
+	  });
+
+	  for (auto i: strings){
+		  cout << i << ' ';
 	  }
   } else {
 	  return 1;
-  }
-
-  sort(array.begin(), array.end(), [](int a, int b){
-	  if (abs(a) < abs(b)){
-		  return true;
-	  } else {
-		  return false;
-	  }
-  });
-
-  for (auto i: array){
-	  cout << i << ' ';
   }
 
   return 0;
