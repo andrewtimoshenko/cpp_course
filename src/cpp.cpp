4968b365f9b7e5a8a09804e650ba446186964943
Andrew Timoshenko <andrewtimoshenko@klika-tech.com>
2019-05-14 23:01:43 +0300

    [WHITE, week 3]. Sorted strings

--- src/cpp.cpp
+++ src/cpp.cpp
@@ -1,50 +1,17 @@
-#include <iostream>
-#include <string>
-#include <map>
-#include <algorithm>
-#include <vector>
-#include <set>
 
-using namespace std;
-
-int main() {
-  int n;
-  cin >> n;
-  /*
-   * Не нужно ничего усложнять!
-   * Просто нужно использовать те вещи, которые дали в уроках
-   * и не придумывать лишнего!
-   */
-  vector <string> strings;
-  if (n >= 0 && n <= 1000){
-	  for (int i = 0; i < n; ++i) {
-		  string str;
-		  cin >> str;
-		  if (str.size() >= 1 && str.size() <= 15){
-			  strings.push_back(str);
-		  }
-	  }
-
-	  sort(strings.begin(), strings.end(), [](string a, string b){ // a.size() > b.size() &&
-		  for (auto &ch: a){
-			  ch = tolower(ch);
-		  }
-		  for (auto &ch: b){
-			  ch = tolower(ch);
-		  }
-		  if(a < b) {
-			  return true;
-		  } else {
-			  return false;
-		  }
-	  });
-
-	  for (auto i: strings){
-		  cout << i << ' ';
-	  }
-  } else {
-	  return 1;
+class SortedStrings {
+public:
+  void AddString(const string& s) {
+    // добавить строку s в набор
+	  str.push_back(s);
   }
+  vector<string> GetSortedStrings() {
+    // получить набор из всех добавленных строк в отсортированном порядке
+	  sort(str.begin(), str.end());
+	  return str;
+  }
+private:
+  // приватные поля
+  vector <string> str;
+};
 
-  return 0;
-}
